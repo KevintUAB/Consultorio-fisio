e642aa9fb50f3e7cfff91afb76ab5625863ae473
KevintUAB <kevin.toledo@uab.edu.bo>
2026-07-30 21:44:33 -0400

    MODO TEMA

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const botonModo = document.getElementById("modo");
 
+botonModo.addEventListener("click", function(){
+
+    document.body.classList.toggle("dark-mode");
+
+    if(document.body.classList.contains("dark-mode")){
+
+        botonModo.innerHTML="☀️ Modo claro";
+
+    }else{
+
+        botonModo.innerHTML="🌙 Modo oscuro";
+
+    }
+
+});
